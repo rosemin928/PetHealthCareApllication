9892ae6cb5eb8824ea51a3439a3fdf8fd2d6637d
rosemin928 <121722789+rosemin928@users.noreply.github.com>
2024-10-07 23:20:16 +0900

    2024-10-07

--- app/src/main/java/com/example/pethealthapplication/SnsFragment.kt
+++ app/src/main/java/com/example/pethealthapplication/SnsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.pethealthapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 
 class SnsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +14,16 @@ class SnsFragment : Fragment() {
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sns, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_sns, container, false)
+
+        //게시물 작성 페이지로 이동
+        val writeButton = view.findViewById<RelativeLayout>(R.id.writeBtn)
+        writeButton.setOnClickListener{
+            val intent = Intent(requireContext(), SnsWriteActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 }
---
+++ app/src/main/java/com/example/pethealthapplication/SnsWriteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pethealthapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SnsWriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sns_write)
+    }
+}
